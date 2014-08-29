49124509a0b6d1db701da876353a3b5c7098112c
HammadH <hammadsyed9@gmail.com>
2014-08-29 17:30:22 +0400

    first release

--- foodcook/foodcook/settings.py
+++ foodcook/foodcook/settings.py
@@ -71,8 +71,13 @@ WSGI_APPLICATION = 'foodcook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': '44cooksdb',
+        'USER': 'hammad',
+        'PASSWORD': 'quakeroats',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+
     }
 }
 
